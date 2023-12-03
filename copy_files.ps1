4cf0ef4b2c567d086729518d544b5ee3612badf2
Vlad is Law <nandate@yandex.ru>
2023-12-03 03:19:15 +0300

    Update copy_files.ps1

--- copy_files.ps1
+++ copy_files.ps1
@@ -6,11 +6,11 @@ Clear-Host
 $Host.UI.RawUI.WindowTitle = "Copy Files Script ($version)"
 Write-Host "Loading..."
 
-# '$PSScriptRoot' - the location of this program's directory
+# Define initialPath and destinationPath
 $initialPath = "$PSScriptRoot\.minecraft\resourcepacks"
 $destinationPath = "$PSScriptRoot\..\MI-NH\.minecraft\resourcepacks"
 
-# Copy files from initialPat to destinationPath
+# Copy files from initialPath to destinationPath
 Copy-Item -Path "$initialPath\*" -Destination "$destinationPath" -Recurse -Force
 
 # Notification of work successfully finished
